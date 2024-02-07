790af6e520f5fe8b57acc294454e887201a668f8
EetuLas <n3laee00@students.oamk.fi>
2024-02-07 16:20:02 +0200

    Valmis

---
+++ js/functions.js
@@ -0,0 +1,41 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+let corr = 0
+let incorr = 0
+
+document.querySelector('#correct').innerHTML = corr
+document.querySelector('#incorrect').innerHTML = incorr
+
+const getRandomOntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomOntNumberInRange(1,10)
+    rand_num2 = getRandomOntNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+})
+
+document.querySelector('button').addEventListener('click', () => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('Correct!')
+        corr++;
+        document.querySelector('#correct').innerHTML = corr
+    } else {
+        alert('Incorrect!')
+        incorr++;
+        document.querySelector('#incorrect').innerHTML = incorr
+    }
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+})
